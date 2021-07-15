f8413c1ece8e3523bf0fcc024103fd93feef66b2
versianidev <michel@onfly.com.br>
2021-07-15 14:45:06 -0300

    New - server.js implemented , and .env installed

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Testiiing!')
+require('dotenv').config();
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+    identity: {
+        //TODO : Fazer o .env com as credenciais.
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_BOT_USERNAME
+    },
+    channels: [ 'forgivenbr' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    console.log(`${tags['display-name']}: ${message}`);
+});
